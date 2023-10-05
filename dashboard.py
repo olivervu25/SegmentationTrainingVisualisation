7b1a61c8fdde55271d10b96c356d4f481c316f69
olivervu25 <longvu2000.work@gmail.com>
2023-10-05 17:06:35 +1000

    first commit

---
+++ dashboard.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Your imports and utility functions here...
+
+def simulate_training():
+    # Your code to simulate training over epochs
+    # Including updating plots and other visual elements
+    pass
+
+st.title('Model Training Visualization')
+
+if st.button('Start Training'):
+    simulate_training()
+
+    # Display your plots, metrics, etc.
+    # For instance:
+    # fig1, fig2, ... = generate_your_plots()
+    # st.pyplot(fig1)
+    # st.pyplot(fig2)
+    # ... and so on
